a182e49943218599de0f8e089f8504e11cf7f360
Yassine Oularbi <yassineoularbi4@gmail.com>
2024-11-27 05:46:53 +0100

    hero section

--- js/script.js
+++ js/script.js
@@ -1,17 +1,94 @@
 function toggleColorMode() {
-    const colorModeButton = document.querySelector('.color-mode');
     const body = document.body;
+    const modeToggler = document.querySelector('.color-mode .tooltip');
+    const moonIcon = document.querySelector('.color-mode i');
+    const logo = document.getElementById('logo');
 
-    if (colorModeButton) {
-        colorModeButton.addEventListener('click', () => {
-            const colorModeIcon = document.querySelector('.color-mode-icon');
-            if (colorModeIcon) {
-                colorModeIcon.classList.toggle('active');
-            }
-            body.classList.toggle('dark-mode');
-        });
+    body.classList.toggle('light-mode');
+
+    if (body.classList.contains('light-mode')) {
+        document.documentElement.style.setProperty('--black-color', '#fff');
+        document.documentElement.style.setProperty('--white-color', '#000');
+        modeToggler.textContent = 'Dark mode';
+        moonIcon.classList.remove('fa-cloud-moon');
+        moonIcon.classList.add('fa-cloud-sun');
+        localStorage.setItem('theme', 'light');
+        logo.src = 'images/logo/logo-black.png';
+    } else {
+        document.documentElement.style.setProperty('--black-color', '#000');
+        document.documentElement.style.setProperty('--white-color', '#fff');
+        modeToggler.textContent = 'Light mode';
+        moonIcon.classList.remove('fa-cloud-sun');
+        moonIcon.classList.add('fa-cloud-moon');
+        localStorage.setItem('theme', 'dark');
+        logo.src = 'images/logo/logo-white.png';
     }
 }
+
+function loadTheme() {
+    const savedTheme = localStorage.getItem('theme');
+    const body = document.body;
+    const modeToggler = document.querySelector('.color-mode .tooltip');
+    const moonIcon = document.querySelector('.color-mode i');
+    const logo = document.getElementById('logo');
+
+    if (savedTheme === 'light') {
+        body.classList.add('light-mode');
+        document.documentElement.style.setProperty('--black-color', '#fff');
+        document.documentElement.style.setProperty('--white-color', '#000');
+        modeToggler.textContent = 'Dark mode';
+        moonIcon.classList.remove('fa-cloud-moon');
+        moonIcon.classList.add('fa-cloud-sun');
+        logo.src = 'images/logo/logo-black.png';
+    } else {
+        body.classList.remove('light-mode');
+        document.documentElement.style.setProperty('--black-color', '#000');
+        document.documentElement.style.setProperty('--white-color', '#fff');
+        modeToggler.textContent = 'Light mode';
+        moonIcon.classList.remove('fa-cloud-sun');
+        moonIcon.classList.add('fa-cloud-moon');
+        logo.src = 'images/logo/logo-white.png';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', loadTheme);
+
+function toggleFullScreen() {
+    let fullScreen = document.getElementById('about');
+    let screenIcon = document.querySelector('.screen i');
+    let screenTooltip = document.querySelector('.screen .tooltip');
+
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen()
+            .catch(err => {
+                console.error(`Erreur lors de l'activation du plein écran : ${err.message}`);
+            });
+        fullScreen.classList.add('full');
+        
+        screenIcon.classList.remove('fa-expand');
+        screenIcon.classList.add('fa-compress');
+        screenTooltip.textContent = 'Exit full screen';
+    } else {
+        document.exitFullscreen()
+            .catch(err => {
+                console.error(`Erreur lors de la sortie du plein écran : ${err.message}`);
+            });
+    }
+}
+
+document.addEventListener('fullscreenchange', function() {
+    let fullScreen = document.getElementById('about');
+    let screenIcon = document.querySelector('.screen i');
+    let screenTooltip = document.querySelector('.screen .tooltip');
+
+    if (!document.fullscreenElement) {
+        fullScreen.classList.remove('full');
+        
+        screenIcon.classList.remove('fa-compress');
+        screenIcon.classList.add('fa-expand');
+        screenTooltip.textContent = 'Full screen';
+    }
+});
 
 
 // function initializeCarousel() {
