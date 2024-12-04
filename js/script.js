0d02fffa9bacd2bb7ee1291685f7a2fdcc5488eb
Yassine Oularbi <yassineoularbi4@gmail.com>
2024-12-04 12:57:22 +0100

    Add smooth behavior scroll

--- js/script.js
+++ js/script.js
@@ -395,3 +395,18 @@ function toggleSkills(button) {
         }
     });
 }
+
+document.querySelectorAll('.navbar_item_link').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth', 
+                block: 'start'
+            });
+        }
+    });
+});
