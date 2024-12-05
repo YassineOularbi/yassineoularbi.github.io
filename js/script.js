52f9a4a898115623e8e3cd36a06b89ab5f91189a
Yassine Oularbi <yassineoularbi4@gmail.com>
2024-12-05 06:12:54 +0100

    Add media query for screen between 900px & 800px

--- js/script.js
+++ js/script.js
@@ -397,14 +397,14 @@ function toggleSkills(button) {
 }
 
 document.querySelectorAll('.navbar_item_link').forEach(link => {
-    link.addEventListener('click', function(e) {
+    link.addEventListener('click', function (e) {
         e.preventDefault();
         const targetId = this.getAttribute('href').substring(1);
         const targetElement = document.getElementById(targetId);
-        
+
         if (targetElement) {
             targetElement.scrollIntoView({
-                behavior: 'smooth', 
+                behavior: 'smooth',
                 block: 'start'
             });
         }
